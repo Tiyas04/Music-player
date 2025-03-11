cfb909bc3dd3223ce7e44c0587f20d71a9ad0efc
Tiyas04 <mandaltiyas2410@gmail.com>
2025-03-11 15:38:47 +0530

    Changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let audio1 = new Audio(`I Wanna Be Yours.mp3`)
+let audio = new Audio(`I Wanna Be Yours.mp3`)
 let isPlay = false
 let img = document.body.getElementsByClassName(`image`)[0]
 let name = document.body.getElementsByClassName(`name`)[0]
@@ -6,38 +6,38 @@ let volumeup = document.body.getElementsByClassName(`high-volume`)[0]
 let volumedown = document.body.getElementsByClassName(`volume-down`)[0]
 let playbutton = document.body.getElementsByClassName(`play`)[0]
 let player = document.body.querySelector(`#player`)
-player.max = audio1.duration
-player.value = audio1.currentTime
+player.max = audio.duration
+player.value = audio.currentTime
 name.innerHTML = `I Wanna Be Yours`
 const music = () => {
     playbutton.addEventListener("click", () => {
         if (isPlay === false) {
-            audio1.play()
+            audio.play()
             playbutton.src = `video-pause-button.png`
             setInterval(() => {
-                player.value = audio1.currentTime
-                player.max = audio1.duration
+                player.value = audio.currentTime
+                player.max = audio.duration
             }, 50);
             isPlay = true
         }
         else {
-            audio1.pause()
+            audio.pause()
             playbutton.src = `play.png`
             isPlay = false
         }
     })
     player.addEventListener("input",() =>{
-        audio1.play()
-        audio1.currentTime = player.value
+        audio.play()
+        audio.currentTime = player.value
     })
     volumeup.addEventListener("click", () => {
-        if (audio1.volume < 1) {
-            audio1.volume += 0.01
+        if (audio.volume < 1) {
+            audio.volume += 0.01
         }
     })
     volumedown.addEventListener("click", () => {
-        if (audio1.volume > 0) {
-            audio1.volume -= 0.01
+        if (audio.volume > 0) {
+            audio.volume -= 0.01
         }
     })
 }
