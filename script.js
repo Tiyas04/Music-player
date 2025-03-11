127bf505b8f92b35b21fb31dc5d16672c6f5a88a
Tiyas04 <mandaltiyas2410@gmail.com>
2025-03-11 13:50:16 +0530

    [Modfied]

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 let audio1 = new Audio(`I Wanna Be Yours.mp3`)
-let audio2 = new Audio(`Infinity.mp3`)
-let audio3 = new Audio(``)
+let isPlay = false
+let img = document.body.getElementsByClassName(`image`)
+const music = () => {
+    let playbutton = document.getElementsByClassName(`play`)
+    playbutton.addEventListener("click", () => {
+        if (isPlay === false) {
+            audio1.play()
+            playbutton.src = `video-pause-button.png`
+            isPlay = true
+        }
+        else {
+            audio1.pause()
+            playbutton.src = `play.png`
+            isPlay = false
+        }
+    })
+}
+music()
